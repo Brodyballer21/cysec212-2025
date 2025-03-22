3850f4b148d76b892735b290908dce188f8ee777
Brodyballer21 <paris012@gannon.edu>
2025-03-22 00:33:36 -0400

    powerReceiver in main

--- Quiz01Application/app/src/main/java/edu/gannon/quiz01application/MainActivity.java
+++ Quiz01Application/app/src/main/java/edu/gannon/quiz01application/MainActivity.java
@@ -1,11 +1,15 @@
 package edu.gannon.quiz01application;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -38,4 +42,18 @@ public class MainActivity extends AppCompatActivity {
         registerReceiver(airplaneModeReceiver, new IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED));
         registerReceiver(networkReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
     }
+
+    private final BroadcastReceiver powerReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+            isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING || status == BatteryManager.BATTERY_STATUS_FULL;
+
+            tvChargingStatus.setText("Charging: " + (isCharging ? "Yes" : "No"));
+            
+            Toast.makeText(context, isCharging ? "Charging" : "Not Charging", Toast.LENGTH_SHORT).show();
+
+            updateBackgroundColor();
+        }
+    };
 }
